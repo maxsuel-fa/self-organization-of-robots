340a91ceea13a792561fbbc6b124eb736ce8cf9f
maxsuel-fa <maxsuelalmeida89@gmail.com>
2025-03-11 12:34:46 +0100

    agents.py: Adding a base classe for the agents

---
+++ src/agents.py
@@ -0,0 +1,72 @@
+"""
+File: agents.py
+Brief: Implements the Robot agents classes
+Authors: 
+"""
+
+class Agent:
+    """
+    Base class for the agents.
+    """
+    def __init__(self) -> None:
+        """
+        TODO
+        """
+        pass
+
+    
+    def _percepts(self):
+        """
+        TODO
+        """
+        pass
+
+
+    def _deliberate(self):
+        """
+        TODO
+        """
+        pass
+
+
+    def _do(self):
+        """
+        TODO
+        """
+        pass
+
+
+
+
+class GreenAgent(Agent):
+    """
+    TODO
+    """
+    def __init__(self) -> None:
+        """
+        TODO
+        """
+        pass
+
+
+
+class YellowAgent(Agent):
+    """
+    TODO
+    """
+    def __init__(self) -> None:
+        """
+        TODO
+        """
+
+
+class RedAgent(Agent):
+    """
+    TODO
+    """
+    def __init__(self) -> None:
+        """
+        TODO
+        """
+        pass
+
